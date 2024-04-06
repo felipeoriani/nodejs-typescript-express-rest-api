ba134fc7e1b31e69e26ce04dcef4ea133787930d
Felipe Oriani <felipeoriani@gmail.com>
2024-04-06 17:00:51 -0300

    Add application bootstrap with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import express from 'express'
+import routes from './routes'
+import { logger } from './core/infrastructure/logger'
+import { createExpressLogger } from './middlewares/express-logger'
+import { errorHandler } from './middlewares/express-error-handler'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(createExpressLogger())
+app.use(express.json())
+app.use(routes)
+app.use(errorHandler)
+
+app.listen(port, () => logger.info(`Task Service running at ${port}.`))
