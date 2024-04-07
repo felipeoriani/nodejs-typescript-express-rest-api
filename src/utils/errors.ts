d49d666c45bbd5b28f5fc40f286490c9785cf29e
Felipe Oriani <felipeoriani@gmail.com>
2024-04-07 18:40:26 -0300

    Refactoring errors file

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,27 +2,38 @@ export class AppError extends Error {
   status: number
   errors?: string[]
 
-  constructor(message: string, status: number, errors?: string[]) {
+  constructor(message: string, status: number) {
     super(message)
     this.status = status
-    this.errors = errors
   }
 }
 
 export class BadRequestError extends AppError {
-  constructor(message: string, errors?: string[]) {
-    super(message, 400, errors)
+  constructor(message: string) {
+    super(message, 400)
   }
 }
 
 export class UnprocessableEntityError extends AppError {
-  constructor(message: string, errors?: string[]) {
-    super(message, 422, errors)
+  constructor(message: string) {
+    super(message, 422)
   }
 }
 
 export class NotFoundError extends AppError {
-  constructor(message: string, errors?: string[]) {
-    super(message, 400, errors)
+  constructor(message: string) {
+    super(message, 404)
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401)
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string) {
+    super(message, 403)
   }
 }
