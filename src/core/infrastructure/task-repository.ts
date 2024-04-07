e3b1f083097d836e01d633af975c5a25f48420d3
Felipe Oriani <felipeoriani@gmail.com>
2024-04-07 18:36:44 -0300

    Add task repository

--- src/core/infrastructure/task-repository.ts
+++ src/core/infrastructure/task-repository.ts
@@ -1,53 +1,97 @@
-import { Task, TaskRepository } from '../domain/task'
+import { Task as TaskDb } from '@prisma/client'
+import { Task, TaskStatus, TaskRepository } from '../domain/task'
+import { getDbClient } from './db/prisma'
+import { CurrentUser } from '../domain/user'
+import { CurrentSession } from '../../utils/session'
 
-export class TaskInMemoryRepository implements TaskRepository {
-  static tasks: Task[] = []
-
-  get tasks(): Task[] {
-    return TaskInMemoryRepository.tasks
-  }
+export class TaskPrismaRepository implements TaskRepository {
+  constructor(
+    private readonly prismaClient = getDbClient(),
+    private readonly session: CurrentUser = new CurrentSession()
+  ) {}
 
   async get(id: string): Promise<Task | null> {
-    const task = this.tasks.filter((x) => x.id === id)
-    if (task) {
-      return task[0]
+    const where: { id: string; userId?: string } = { id }
+
+    if (!this.session.user.super) {
+      where.userId = this.session.user.id
     }
-    return null
+
+    const task = await this.prismaClient.task.findFirst({
+      where,
+    })
+
+    if (!task) {
+      return null
+    }
+
+    return this.MapToDomainModel(task)
   }
 
   async getAll(): Promise<Task[]> {
-    return this.tasks
+    const where: { userId?: string } = {}
+
+    if (!this.session.user.super) {
+      where.userId = this.session.user.id
+    }
+
+    const tasks = await this.prismaClient.task.findMany({
+      where,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return tasks.map(this.MapToDomainModel)
   }
 
-  async create(task: Task): Promise<Task> {
-    this.tasks.push(task)
-    return task
+  async create(task: Omit<Task, 'id'>): Promise<Task> {
+    const taskDb = await this.prismaClient.task.create({
+      data: {
+        title: task.title,
+        description: task.description,
+        status: TaskStatus.Done,
+        userId: this.session.user.id,
+      },
+    })
+
+    return this.MapToDomainModel(taskDb)
   }
 
   async update(id: string, task: Task): Promise<Task | null> {
-    const deleteResult = await this.delete(id)
+    const taskDb = await this.prismaClient.task.update({
+      where: {
+        id,
+        userId: this.session.user.id,
+      },
+      data: {
+        title: task.title,
+        description: task.description,
+        status: task.status,
+      },
+    })
 
-    if (deleteResult) {
-      task.id = id
-      this.tasks.push(task)
-
-      return task
-    }
-
-    return null
+    return this.MapToDomainModel(taskDb)
   }
 
   async delete(id: string): Promise<boolean> {
-    const existingTask = await this.get(id)
-    if (existingTask) {
-      const index = this.tasks.findIndex((task) => task.id === id)
-      if (index !== -1) {
-        this.tasks.splice(index, 1)
-      }
+    await this.prismaClient.task.delete({
+      where: {
+        id,
+        userId: this.session.user.id,
+      },
+    })
+    return true
+  }
 
-      return true
+  private MapToDomainModel(task: TaskDb): Task {
+    return {
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      createdAt: task.createdAt,
+      userId: task.userId,
+      status: <TaskStatus>task.status,
     }
-
-    return false
   }
 }
