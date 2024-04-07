eaa383ae3287467be3f51e472a173a5409c83d9d
Felipe Oriani <felipeoriani@gmail.com>
2024-04-07 18:39:27 -0300

    refactoring the express error middleware

--- src/middlewares/express-error-handler.ts
+++ src/middlewares/express-error-handler.ts
@@ -2,15 +2,16 @@ import { Request, Response, NextFunction } from 'express'
 import { AppError } from '../utils/errors'
 import { logger } from '../core/infrastructure/logger'
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (res.headersSent) {
-    return next(err)
+export const errorMiddleware = (error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (response.headersSent) {
+    return next(error)
   }
 
-  if (err instanceof AppError) {
-    res.status(err.status).send({ error: err.message })
-  } else {
-    logger.error(`An error ocurred while trying to execute ${req.method} - ${req.url}.`, err)
-    res.status(500).send()
+  if (error instanceof AppError) {
+    response.status(error.status).send({ error: error.message })
+    return
   }
+
+  logger.error(`An error ocurred while trying to execute ${request.method} - ${request.url}.`, error)
+  response.status(500).send(error)
 }
