4962bcde3b049857e43d16a2d4f64d48cf59b23d
Felipe Oriani <felipeoriani@gmail.com>
2024-04-07 18:35:15 -0300

    Add migrations for prisma

--- prisma/migrations/20240407161124_init/migration.sql
+++ prisma/migrations/20240407161124_init/migration.sql
@@ -26,5 +26,11 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
 -- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
