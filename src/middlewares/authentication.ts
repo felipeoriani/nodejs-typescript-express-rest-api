593e69da76e06f7b5bcbc65d55ec7d22ad5bee45
Felipe Oriani <felipeoriani@gmail.com>
2024-04-07 18:38:38 -0300

    Add authentication middleware

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import { UserSession } from '../core/domain/user'
+import { config } from '../utils/config'
+import { AppError, UnauthorizedError } from '../utils/errors'
+import { setSession } from '../utils/session'
+
+async function validate(token: string): Promise<UserSession | jwt.VerifyErrors | null> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, config.tokenSecret as jwt.Secret, (error, decoded) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      resolve(decoded as UserSession | jwt.VerifyErrors | null)
+    })
+  })
+}
+
+export const authenticationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authorizationHeader = req.headers.authorization
+
+    if (!authorizationHeader) {
+      throw new UnauthorizedError('An authentication header is required and was not found.')
+    }
+
+    const tokenParts = authorizationHeader.split(' ')
+    if (tokenParts.length !== 2) {
+      throw new UnauthorizedError('An authentication request header must provide a scheme and a token.')
+    }
+
+    const scheme = tokenParts[0]
+    const token = tokenParts[1]
+
+    if (!scheme || scheme.toLowerCase() !== 'bearer' || !token) {
+      throw new UnauthorizedError('An authentication requires a bearer token.')
+    }
+
+    const session = (await validate(token)) as UserSession
+
+    setSession(session, () => {
+      next()
+      return Promise.resolve()
+    })
+  } catch (err) {
+    const error = err as unknown as Error
+
+    if (error instanceof AppError) {
+      next(error)
+    } else {
+      next(new UnauthorizedError(`An error occurred while trying to authenticate the user: ${error.message}.`))
+    }
+  }
+}
