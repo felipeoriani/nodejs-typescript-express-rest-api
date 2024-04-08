247b7b7a1e6c549066ffab4830c767b8367691d0
Felipe Oriani <felipeoriani@gmail.com>
2024-04-08 14:16:46 -0300

    Set session and unauthorized error when no logged on graphql endpoint

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,10 +1,15 @@
 import { getDbClient } from '../core/infrastructure/db/prisma.js'
+import { UnauthorizedError } from '../utils/errors.js'
 import { getSession } from '../utils/session.js'
 
 export const resolvers = {
   Query: {
     tasks: () => {
       const session = getSession()!
+
+      if (!session) {
+        throw new UnauthorizedError('Authentication required')
+      }
 
       if (session.user.super) {
         return getDbClient().task.findMany()
